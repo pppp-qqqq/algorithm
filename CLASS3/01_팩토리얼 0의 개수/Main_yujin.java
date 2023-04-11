d134d9d13d9f682b950eed1f790453db1aa3af1f
Kathleen Jung <ovo0325@outlook.kr>
2023-04-12 07:36:13 +0900

    [1676] 팩토리얼 0의 개수

---
+++ CLASS3/01_팩토리얼 0의 개수/Main_yujin.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1676 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int num = Integer.parseInt(br.readLine());
+		int count = 0;
+ 
+		while (num >= 5) {
+			count += num / 5;
+			num /= 5;
+		}
+		System.out.println(count);
+	}
+
+}
