3d7a35573b538cbd9304670638370bd8903ac9a9
JuEun <86585240+kimjueun1@users.noreply.github.com>
2023-04-12 21:13:59 +0900

    Add files via upload

---
+++ CLASS3/01_팩토리얼 0의 개수/1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int count=0;
+    while(n>=5){
+      count += n/5;
+      n /= 5;
+    }
+    System.out.println(count);
+  }
+}
